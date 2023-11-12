169c3d884a41475bbeab7dd322de7f27ef6f7e37
Nripendra Thakur <nrpndr@users.noreply.github.com>
2023-11-12 10:28:09 +0530

    Adding some movies data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,41 @@
+INSERT IGNORE INTO `db_movies`.`movies` (
+	id, 
+	title, 
+	release_date, 
+	genre, 
+	movie_duration_in_minutes, 
+	director, 
+	description,
+	cast
+	)
+VALUES (
+	1, 
+	'Jawan',
+	'07/09/2023',
+	'Action Thriller',
+	'169',
+	'Atlee',
+	'A high-octane action thriller which outlines the emotional journey of a man who is set to rectify the wrongs in the society.',
+	'Shah Rukh Khan,Nayanthara,Vijay Sethupathi'
+	);
+
+INSERT IGNORE INTO `db_movies`.`movies` (
+	id, 
+	title, 
+	release_date, 
+	genre, 
+	movie_duration_in_minutes, 
+	director, 
+	description,
+	cast
+	)
+VALUES (
+	2, 
+	'Pathaan',
+	'25/01/2023',
+	'Action Thriller',
+	'146',
+	'Siddharth Anand',
+	'An Indian agent races against a doomsday clock as a ruthless mercenary, with a bitter vendetta, mounts an apocalyptic attack against the country.',
+	'Shah Rukh Khan,Deepika Padukone,John Abraham'
+	);
